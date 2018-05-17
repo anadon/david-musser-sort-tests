bd79356d8e18f810ac76d803f8077209f8da5757
Josh Marshall <joshua.r.marshall.1991@gmail.com>
2018-05-17 17:48:59 -0400

    Updated; now compiles, but getting anomalous results.

---
+++ counting.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <algorithm>
+#include <random>
+#include <string>
+#include <vector>
+
+#include "distcount.h"
+#include "itercount.h"
+
+const int number_of_trials = 7;
+
+static std::vector<std::string> headings =
+  {" Introsort",
+   " Heapsort"};
+
+template <class Container>
+class counting {
+public:
+
+  typedef distance_counter<
+    typename Container::iterator,
+    typename Container::difference_type> distance;
+
+  typedef iteration_counter<
+    typename Container::iterator,
+    typename Container::value_type,
+    typename Container::value_type&,
+    distance> iterator;
+
+  static void algorithm(int k, Container& x)
+  {
+    switch (k) {
+    case 1: std::partial_sort(iterator(x.begin()),
+                         iterator(x.end()),
+                         iterator(x.end()));
+    break;/*
+    case 1: introsort(iterator(x.begin()),
+                      iterator(x.end()));
+      break;//*/
+    case 0: std::sort(iterator(x.begin()),
+                      iterator(x.end()));
+      break;
+    }
+  }
+
+};
